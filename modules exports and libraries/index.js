d972f2f3aa3ebf8d59e71cad67bb31499eb7754b
Aryan Choudhary <aryanchoudhary3232@gmail.com>
2024-07-02 22:29:19 +0530

    first commit of hotel

---
+++ modules exports and libraries/index.js
@@ -0,0 +1,9 @@
+const _ = require('lodash');
+const notes = require('./notes.js')
+
+let arr = [1,1,2,3,'hi', 'hi'];
+// console.log(_.uniq(arr));
+// console.log(_.isString(234))
+console.log(notes.age);
+const result = notes.addNumber(23,12);
+console.log(result);
---
+++ modules exports and libraries/notes.js
@@ -0,0 +1,12 @@
+console.log('Hi, I aM Aryan');
+
+const age = 32;
+
+function addNumber(a,b) {
+    return a+b;
+}
+
+module.exports = {
+    age,
+    addNumber
+}
