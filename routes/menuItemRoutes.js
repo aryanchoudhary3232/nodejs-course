6b320e6e1adde34fb8514cfc26b2989d8118bae3
Aryan Choudhary <aryanchoudhary3232@gmail.com>
2024-07-03 14:49:10 +0530

    move all folders to main root nodejs

---
+++ routes/menuItemRoutes.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const MenuItem = require('../../models/MenuItem');
+const Person = require('../../models/person');
+
+router.post('/', async (req,res) => {
+
+    try {
+        const data = req.body;
+
+        const newPerson = new MenuItem(data);
+
+        const response = await newPerson.save();
+        console.log('data saved');
+        res.status(200).json(response);
+
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal server error'});
+    }
+})
+
+router.get('/', async(req,res) => {
+    try{
+        const data = await MenuItem.find();
+        console.log('data fetched');
+        res.status(200).json(data);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal server error'});
+    }
+})
+
+router.get('/:taste', async (req,res) => {
+    try {
+        const tastePara = req.params.taste;
+        if(tastePara === 'sour' || tastePara === 'sweet' || tastePara === 'spicy') {
+            const response = await MenuItem.find({taste: tastePara});
+            console.log('data fetched');
+            res.status(200).json(response);
+        } else {
+            res.status(404).json({error: 'Invalid work type'});
+        }
+    }catch(err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal server error'});
+    }
+})
+
+
+
+module.exports = router;
+
+
