a6bd61837034d9ccec37279a85901c6a2953f345
Aryan Choudhary <aryanchoudhary3232@gmail.com>
2024-07-03 18:02:50 +0530

    after completing menuItem

--- routes/menuItemRoutes.js
+++ routes/menuItemRoutes.js
@@ -47,7 +47,39 @@ router.get('/:taste', async (req,res) => {
     }
 })
 
+router.put('/:id', async(req,res) => {
+    try {
+        const dishId = req.params.id;
+        const updateData = req.body;
+        const response = await MenuItem.findByIdAndUpdate(dishId, updateData, {
+            new: true,
+            runValidators: true,
+        })
+        if(!response){
+            return res.status(404).json({error: 'taste not found'});
+        }
+        console.log('data updated');
+        res.status(200).json(response);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal server error'});
+    }
+})
 
+router.delete('/:id', async(req,res) => {
+    try {
+        const dishId = req.params.id;
+        const response =await  MenuItem.findByIdAndDelete(dishId);
+        if(!response){
+            return res.status(404).json({error: 'taste not found'});
+        }
+        console.log('data deleted successfully');
+        res.status(200).json({message: 'Dish deleted successfully'});
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal server error'});
+    }
+})
 
 module.exports = router;
 
