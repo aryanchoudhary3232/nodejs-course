463b8dc3397a91ec08186ff5c0c32f965fecad94
Aryan Choudhary <aryanchoudhary3232@gmail.com>
2024-07-03 14:33:45 +0530

    after removing modules folder

--- modules exports and libraries/index.js
+++
@@ -1,9 +0,0 @@
-const _ = require('lodash');
-const notes = require('./notes.js')
-
-let arr = [1,1,2,3,'hi', 'hi'];
-// console.log(_.uniq(arr));
-// console.log(_.isString(234))
-console.log(notes.age);
-const result = notes.addNumber(23,12);
-console.log(result);
