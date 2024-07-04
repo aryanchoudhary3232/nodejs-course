a16c3162205a2ae3faa9f167091a238decd2a4ee
Aryan Choudhary <aryanchoudhary3232@gmail.com>
2024-07-04 12:51:02 +0530

    after authentication and authorization

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const personSchema = new mongoose.Schema({
     name: {
@@ -28,8 +29,49 @@ const personSchema = new mongoose.Schema({
     salary: {
         type: Number,
         required: true
-    }
+    },
+    username: {
+        required: true,
+        type: String,
+
+    },
+    password: {
+        required: true,
+        type: String,
+    },
 });
+
+personSchema.pre('save', async function(next) {
+    const person = this;
+
+    //Hash the password only if it is modified or new
+    if(!person.isModified('password')) return next();
+
+    try {
+        //hash salt
+        const salt = await bcrypt.genSalt(10);
+
+        //hash password
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+
+        //Overwrite the plain password with hashed one
+        person.password = hashedPassword;
+
+        next();
+    } catch (err) {
+        next(err);
+    }
+})
+
+personSchema.methods.comparedPassword = async function(candidatePassword){
+    try {
+        //Use bcrypt to compare the provided password with the hashed password
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    } catch (err) {
+        throw err;
+    }
+}
 
 const Person = mongoose.model('Person', personSchema);
 module.exports = Person;
--- server.js
+++ server.js
@@ -2,23 +2,30 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 require('dotenv').config();
+const passport = require('./auth');
+const personRoutes = require('./routes/personRoutes');
+const menuItemRoutes = require('./routes/menuItemRoutes');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
+//Middleware function
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request mode to : ${req.originalUrl}`);
+    next();
+}
 
-const Person = require('./models/person');
-const MenuItem = require('./models/MenuItem');
+app.use(logRequest);
+
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local', {session: false});
 
 app.get('/', (req,res) => {
     res.send('Hi, I am Aryan, Welcome to Hotel');
 })
 
-const personRoutes = require('./routes/personRoutes');
-const menuItemRoutes = require('./routes/menuItemRoutes');
-
-app.use('/person', personRoutes);
+app.use('/person', localAuthMiddleware, personRoutes);
 app.use('/menu', menuItemRoutes);
 
 
